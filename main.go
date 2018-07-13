f392034875d2026cad84629d3a9cb3185692508e
pheever <phivos.phivou@gmail.com>
2018-07-13 23:34:14 +0300

    created basic http server

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
-	fmt.Println("hello gym")
-	fmt.Println("another change")
-	fmt.Println("yet another change")
+	fmt.Println("assigning handlers")
+	http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func home(resw http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(resw, "this is just a message passed at %s", time.Now().String())
 }
