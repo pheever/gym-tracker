30ecc3bd7b634ed88947c3a1826bc6978fe593c5
pheever <phivos.phivou@gmail.com>
2018-07-21 18:44:41 +0300

    removed stuff

--- main.go
+++ main.go
@@ -9,28 +9,11 @@ import (
 )
 
 func main() {
-	/*
-		athletes := make([]Athlete, 0)
-		for i := 0; i < 10; i++ {
-			athletes = append(athletes, MakeNewAthlete(fmt.Sprintf("name-%d", i), fmt.Sprintf("surname-%d", i), time.Now(), false))
-		}
-
-		for i := 0; i < 4; i++ {
-			athletes[4].UpdateCurrentAttributes(112.4+float32(i), 1.74+float32(i))
-		}
-		for i := 0; i < len(athletes[4].Historical); i++ {
-			fmt.Println(i, athletes[4].Historical[i])
-		}
-		fmt.Println(athletes[4].Current)
-	*/
-	//*
 	fmt.Println("assigning handlers")
 	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-	//*/
 }
 
-//*
 func home(resw http.ResponseWriter, req *http.Request) {
 	athl := Athlete{
 		Name:     "phivos",
@@ -46,11 +29,10 @@ func home(resw http.ResponseWriter, req *http.Request) {
 		},
 	}
 
-	fmt.Println("Current attributes:", athl.Current.Weight, athl.Current.Height, athl.Age(), athl.IsAdult())
-
 	tmpl, err := template.New("Athlete").Funcs(template.FuncMap{
 		"Age": athl.Age,
 	}).ParseFiles("html-templates/athlete.html")
+
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -61,5 +43,3 @@ func home(resw http.ResponseWriter, req *http.Request) {
 	}
 
 }
-
-//*/
